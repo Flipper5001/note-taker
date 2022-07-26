0d308bb355f59e804b22203318793dac801d5190
Flipper <filipe@motionfirestudios.com.au>
2022-07-26 12:30:05 +0800

    added server.js file with changes to account for errors and src folder for readme file

---
+++ application/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const apiRoutes = require('./routes/api')
+const webRoutes = require('./routes/web')
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoutes);
+app.use('/', webRoutes);
+
+app.listen(PORT, () =>
+  console.log(`App is running at port:${PORT}`)
+);
